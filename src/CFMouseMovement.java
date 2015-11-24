793f9e2af7755729b1c1ab9a4e5bdd8270c5e430
yoni116 <yoni116@gmail.com>
2015-11-24 15:00:27 +0200

    Controly Server Ver 4.6
    - added support on left and middle mouse clicks

--- src/CFMouseMovement.java
+++ src/CFMouseMovement.java
@@ -25,7 +25,32 @@ public class CFMouseMovement implements Runnable {
     public void run() {
 
 
-//	 NativeMouse nativeMouse = new NativeMouse();
+        //System.out.println(result);
+
+
+        switch (result) {
+            case "leftClick":
+                mouse.mousePress(InputEvent.BUTTON1_MASK);
+                mouse.mouseRelease(InputEvent.BUTTON1_MASK);
+                break;
+            case "rightClick":
+                mouse.mousePress(InputEvent.BUTTON3_MASK);
+                mouse.mouseRelease(InputEvent.BUTTON3_MASK);
+                break;
+            case "middleClick":
+                mouse.mousePress(InputEvent.BUTTON2_MASK);
+                mouse.mouseRelease(InputEvent.BUTTON2_MASK);
+                break;
+            default:
+                mouseMovement(result);
+                break;
+        }
+
+    }
+
+
+    private void mouseMovement(String result) {
+
         Point mousePoint = MouseInfo.getPointerInfo().getLocation();
         x = mousePoint.getX();
         y = mousePoint.getY();
@@ -33,75 +58,66 @@ public class CFMouseMovement implements Runnable {
 
         String[] pointArray;
 
+        pointArray = result.split(",", 2);
 
-        //System.out.println(result);
-
-        if (result.equals("click")) {
-            mouse.mousePress(InputEvent.BUTTON1_MASK);
-            mouse.mouseRelease(InputEvent.BUTTON1_MASK);
-        } else {
-            pointArray = result.split(",", 2);
-
-            // System.out.println(result);
+        // System.out.println(result);
 
 
-            if ((pointArray[0] == null) || (pointArray[1] == null))
-                return;
+        if ((pointArray[0] == null) || (pointArray[1] == null))
+            return;
 
 
-            double recievedY = Double.parseDouble(pointArray[1]);
-            double recievedX = Double.parseDouble(pointArray[0]);
+        double recievedY = Double.parseDouble(pointArray[1]);
+        double recievedX = Double.parseDouble(pointArray[0]);
 
 //calculations
 
 //calculate the distance and determine speed
-            double distance = Math.sqrt((recievedX * recievedX) + (recievedY * recievedY));
-            if (distance <= 200)
-                distance = 1;
+        double distance = Math.sqrt((recievedX * recievedX) + (recievedY * recievedY));
+        if (distance <= 200)
+            distance = 1;
 
-            if (distance > 200 && distance <= 500)
-                distance = 3;
+        if (distance > 200 && distance <= 500)
+            distance = 3;
 
-            if (distance > 500 && distance <= 1000)
-                distance = 30;
+        if (distance > 500 && distance <= 1000)
+            distance = 30;
 
-            if (distance > 1000)
-                distance = 70;
+        if (distance > 1000)
+            distance = 70;
 
 
 //System.out.println("distance is: " + distance);
 
 //calculate the degrees
-            double radians = Math.atan2(recievedY, recievedX);
+        double radians = Math.atan2(recievedY, recievedX);
 
 
 //System.out.println("degrees are : " + degree);
 
 
 //calculate new X and Y destination
-            double Y = Math.sin(radians) * distance;
-            double X = Math.cos(radians) * distance;
+        double Y = Math.sin(radians) * distance;
+        double X = Math.cos(radians) * distance;
 
 //System.out.println("Y = " + Y + " X = " + X);
 
 
-            recievedX = Math.round(X);
-            recievedY = Math.round(Y);
+        recievedX = Math.round(X);
+        recievedY = Math.round(Y);
 
 
-            LOGGER.info("Move Mouse by (" + recievedX + "," + recievedY + ")");
+        LOGGER.info("Move Mouse by (" + recievedX + "," + recievedY + ")");
 
 
-
-            mousePoint = MouseInfo.getPointerInfo().getLocation();
-            x = mousePoint.getX();
-            y = mousePoint.getY();
+        mousePoint = MouseInfo.getPointerInfo().getLocation();
+        x = mousePoint.getX();
+        y = mousePoint.getY();
 //System.out.println(mousePoint);
 
-            mouse.mouseMove((int) (x + recievedX), (int) (y + recievedY));
+        mouse.mouseMove((int) (x + recievedX), (int) (y + recievedY));
 
-            //We need a try-catch because lots of errors can be thrown
-        }
+        //We need a try-catch because lots of errors can be thrown
     }
 
 }
